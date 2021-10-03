8611ab73039731c22c4ff850d95f92fe3bbd5e3d
Luis Diego Quirós Gómez <luis25quiros@gmail.com>
2021-10-03 15:36:04 -0600

    actualización constructor trabajador asalariado

---
+++ src/empleadoAsalariado.cpp
@@ -0,0 +1,8 @@
+#include "empleadoAsalariado.h"
+
+EmpleadoAsalariado::EmpleadoAsalariado(std::string nombre,
+                                       std::string apellido,
+                                       std::string email) : Empleado(nombre, apellido, email)
+{
+
+}
---
+++ src/empleadoAsalariado.h
@@ -0,0 +1,17 @@
+#ifndef EMPLEADOASALARIADO_H
+#define EMPLEADOASALARIADO_H
+
+#include "empleado.h"
+#include <string>
+
+class EmpleadoAsalariado : public Empleado
+{
+
+private:
+    float salarioBruto;
+
+public:
+    EmpleadoAsalariado(std::string nombre, std::string apellido, std::string email);
+};
+
+#endif
