f1ce4f008fb75f030d8ada102312fc58e8ed1d51
Luis Diego Quirós Gómez <luis25quiros@gmail.com>
2021-10-03 15:35:39 -0600

    implementacion clase básica empleado Por Horas

---
+++ src/empleadoPorHoras.cpp
@@ -0,0 +1,8 @@
+#include "empleadoPorHoras.h"
+
+EmpleadoPorHoras::EmpleadoPorHoras(std::string nombre,
+                                       std::string apellido,
+                                       std::string email) : Empleado(nombre, apellido, email)
+{
+    
+}
---
+++ src/empleadoPorHoras.h
@@ -0,0 +1,18 @@
+#ifndef EMPLEADOPORHORAS_H
+#define EMPLEADOPORHORAS_H
+
+#include "empleado.h"
+#include <string>
+
+class EmpleadoPorHoras : public Empleado
+{
+
+private:
+    float tarifaPorHoras;
+    int horasLaboradas;
+
+public:
+    EmpleadoPorHoras(std::string nombre, std::string apellido, std::string email);
+};
+
+#endif
