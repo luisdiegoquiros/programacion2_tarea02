755941a87692b155e903bf174cfb1f33e54a17ef
Luis Diego Quirós Gómez <luis25quiros@gmail.com>
2021-10-04 21:13:34 -0600

    main

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#ifndef UNIT_TEST
+
+#include "empleadoAsalariado.h"
+#include "empleadoPorHoras.h"
+#include "arbol.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    //Crea el árbol
+    Arbol *arbol = new Arbol();
+
+    //Carga la información de todas las personas que trabajan en la compañia.
+    std::ifstream archivoPersonas("C:/Personas.txt", std::ifstream::in);
+
+    if (!archivoPersonas.is_open())
+    {
+        std::cerr << "Error leyendo archivo Personas.txt" << std::endl;
+        return -1;
+    }
+
+    arbol->cargarPersonas(archivoPersonas);
+
+    archivoPersonas.close();
+
+    //Carga la información de los salarios de los empleados asalariados.
+    std::ifstream archivoNomina("C:/Nomina.txt", std::ifstream::in);
+
+    if (!archivoNomina.is_open())
+    {
+        std::cerr << "Error leyendo archivo Nomina.txt" << std::endl;
+        return -1;
+    }
+
+    arbol->cargarSalarios(archivoNomina);
+    archivoNomina.close();
+    //Carga la información de las horas y tarifas para los empleados por horas.
+    std::ifstream archivoHorasTrabajadas("C:/HorasTrabajadas.txt", std::ifstream::in);
+
+    if (!archivoHorasTrabajadas.is_open())
+    {
+        std::cerr << "Error leyendo archivo HorasTrabajadas.txt" << std::endl;
+        return -1;
+    }
+
+    arbol->cargarHorasTrabajadas(archivoHorasTrabajadas);
+    archivoHorasTrabajadas.close();
+
+
+    //Archivo de Salida
+
+    std::ofstream file("C:\\Users\\luis2\\Desktop\\programacion2_tarea02\\reporte.csv", std::ofstream::out);
+
+   arbol->generarReporte(file);
+  
+    file.close();
+    delete arbol;
+
+    return 0;
+}
+
+#endif
